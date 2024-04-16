0e3a4c3454a6d4f88d2f32c03a73efd8491e32c8
Ohddang904 <jury618@naver.com>
2024-04-16 18:11:03 +0900

    20240416

--- picket/src/main/java/com/example/picket/controller/PageInfoController.java
+++ picket/src/main/java/com/example/picket/controller/PageInfoController.java
@@ -1,11 +1,26 @@
 package com.example.picket.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Slf4j
 @Controller
 public class PageInfoController {
     /*concert_info*/
+
+    /* 라 트라비아타 이동 */
+    @GetMapping("/LaTraviata")
+    public String gotoLaTraviata() {
+        return "/category/Classic/LaTraviata";
+    }
+
+    /* 막심 벤게로프 이동 */
+    @GetMapping("/Maxim")
+    public String gotoMaxim() {
+        return "/category/Classic/Maxim";
+    }
+
+
 
     /*musical_info*/
 
