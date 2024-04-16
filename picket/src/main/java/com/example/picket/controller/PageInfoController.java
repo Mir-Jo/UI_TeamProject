4dcac9b0a380f3583cd24102e9f3e71d46481537
Yoon-jae-min <jmyoon1994@naver.com>
2024-04-16 16:57:36 +0900

    info controller append

---
+++ picket/src/main/java/com/example/picket/controller/PageInfoController.java
@@ -0,0 +1,17 @@
+package com.example.picket.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageInfoController {
+    /*concert_info*/
+
+    /*musical_info*/
+
+    /*act_info*/
+
+    /*classic_info*/
+
+    /*exhibit_info*/
+}
