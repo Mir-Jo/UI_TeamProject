b7fc2553054e8399faa94d97dc6725a16c2b2c77
Yoon-jae-min <jmyoon1994@naver.com>
2024-04-16 18:18:49 +0900

    20240416-jm

--- picket/src/main/java/com/example/picket/controller/PageInfoController.java
+++ picket/src/main/java/com/example/picket/controller/PageInfoController.java
@@ -10,6 +10,45 @@ public class PageInfoController {
     /*musical_info*/
 
     /*act_info*/
+    @GetMapping("/musicalTheaterCarolInfo")
+    public String musicalTheaterCarol(){
+        return "/category/Act/musicalTheaterCarol";
+    }
+
+    @GetMapping("/theaterCloserInfo")
+    public String theaterCloser(){
+        return "/category/Act/theaterCloser";
+    }
+
+    @GetMapping("/theaterHangOverInfo")
+    public String theaterHangOver(){
+        return "/category/Act/theaterHangOver";
+    }
+
+    @GetMapping("/theaterHerzKlangInfo")
+    public String theaterHerzKlang(){
+        return "/category/Act/theaterHerzKlang";
+    }
+
+    @GetMapping("/theaterHeungSinSoInfo")
+    public String theaterHeungSinSo(){
+        return "/category/Act/theaterHeungSinSo";
+    }
+
+    @GetMapping("/theaterInHellInfo")
+    public String theaterInHell(){
+        return "/category/Act/theaterInHell";
+    }
+
+    @GetMapping("/theaterPSpartnerInfo")
+    public String theaterPSpartner(){
+        return "/category/Act/theaterPSpartner";
+    }
+
+    @GetMapping("/theaterRainbowInfo")
+    public String theaterRainbow(){
+        return "/category/Act/theaterRainbow";
+    }
 
     /*classic_info*/
 
