9da16668b24befe562436adec1647e32662e6c23
Ddonggirim <choibu02222@naver.com>
2024-04-16 18:09:17 +0900

    최부용

--- picket/src/main/java/com/example/picket/controller/PageInfoController.java
+++ picket/src/main/java/com/example/picket/controller/PageInfoController.java
@@ -1,11 +1,28 @@
 package com.example.picket.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Slf4j
 @Controller
 public class PageInfoController {
     /*concert_info*/
+    @GetMapping("season")
+    public String gotoseason(){return "/category/Concert/season";}
+    @GetMapping("/beautiful")
+    public String gotobeautiful(){return "/category/Concert/beautiful";}
+    @GetMapping("/bobby")
+    public String gotobobby(){return "/category/Concert/bobby";}
+    @GetMapping("/boys")
+    public String gotoboys(){return "/category/Concert/boys";}
+    @GetMapping("/daysix")
+    public String gotodaysix(){return "/category/Concert/daysix";}
+    @GetMapping("/king")
+    public String gotoking(){return "/category/Concert/king";}
+    @GetMapping("/glow")
+    public String gotoglow(){return "/category/Concert/glow";}
+    @GetMapping("/worldtour")
+    public String gotoworldtour(){return "/category/Concert/worldtour";}
 
     /*musical_info*/
 
