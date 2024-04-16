170c7d6ac65ce514929b2d740529739f0cbb71d7
stagechaplin <https://github.com/stagechaplin/stagechaplin.git>
2024-04-16 18:15:28 +0900

    인포 수정 중...

--- picket/src/main/java/com/example/picket/controller/PageInfoController.java
+++ picket/src/main/java/com/example/picket/controller/PageInfoController.java
@@ -8,7 +8,8 @@ public class PageInfoController {
     /*concert_info*/
 
     /*musical_info*/
-
+@GetMapping("/musical_paris.info")
+public String gotoMyPage() { return "/category/Musical/paris";}
     /*act_info*/
 
     /*classic_info*/
