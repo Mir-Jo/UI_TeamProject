cd8b5db943caebf041b5dbee6d660c9a52ded634
Yoon-jae-min <jmyoon1994@naver.com>
2024-04-26 14:31:26 +0900

    20240426

--- picket_thymeleaf/src/main/java/com/example/picket/repository/TicketRepository.java
+++ picket_thymeleaf/src/main/java/com/example/picket/repository/TicketRepository.java
@@ -4,6 +4,6 @@ import com.example.picket.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
+public interface TicketRepository extends JpaRepository<Ticket, String> {
 
 }
