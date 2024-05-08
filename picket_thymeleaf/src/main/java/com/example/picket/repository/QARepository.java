6b909af62efb953f77eaefe91cd64e508361e97b
Mir-Jo <jomire1214@gmail.com>
2024-05-08 17:30:12 +0900

    240508Mirsecond

--- picket_thymeleaf/src/main/java/com/example/picket/repository/QARepository.java
+++ picket_thymeleaf/src/main/java/com/example/picket/repository/QARepository.java
@@ -3,10 +3,15 @@ package com.example.picket.repository;
 import com.example.picket.entity.Customer;
 import com.example.picket.entity.QA;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public interface QARepository extends JpaRepository<QA, Long> {
+    @Query("SELECT qa FROM QA qa WHERE qa.customer.id IN :customerIds")
+    List<QA> findAllByCustomerIds(@Param("customerIds") List<String> customerIds);
 }
